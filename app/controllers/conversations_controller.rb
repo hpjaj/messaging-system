6ea9fb6d62dc9e740b8a70cf861e9912d9a8a38b
hpjaj <hmlevine@gmail.com>
2015-03-18 00:20:38 -0600

    Refactoring.  Abstracted code from the Conversations controller into the Message model.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -6,7 +6,7 @@ class ConversationsController < ApplicationController
 
   def show
     current_conversation = Conversation.find(params[:id])
-    @messages = conversations_sent_and_received_messages(current_conversation, current_user)
+    @messages = Message.conversations_sent_and_received_messages(current_conversation, current_user)
   end
 
   def destroy
@@ -14,14 +14,6 @@ class ConversationsController < ApplicationController
     @conversations.destroy
     flash[:notice] = 'Conversation deleted'
     redirect_to root_path
-  end
-
-private
-  
-  def conversations_sent_and_received_messages(conversation, user)
-    sent_messages = Message.where(user_id: user).where(recipient_id: conversation.recipient_id)
-    received_messages = Message.where(user_id: conversation.recipient_id).where(recipient_id: user)
-    all_messages = ( sent_messages + received_messages ).sort { |x,y| y.created_at <=> x.created_at}
   end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,10 @@ class Message < ActiveRecord::Base
   validates :recipient_id, presence: true
   validates :body, presence: true
 
+  def self.conversations_sent_and_received_messages(conversation, user)
+    sent_messages = self.where(user_id: user).where(recipient_id: conversation.recipient_id)
+    received_messages = self.where(user_id: conversation.recipient_id).where(recipient_id: user)
+    all_messages = ( sent_messages + received_messages ).sort { |x,y| y.created_at <=> x.created_at}
+  end
+
 end
