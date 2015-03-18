e48483880111e8531267c485b8ba32525a175f89
hpjaj <hmlevine@gmail.com>
2015-03-18 16:25:13 -0600

    Added configuration code for Devise.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,9 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # Devise includes some test helpers for functional specs. In order to use them, you need to include Devise in your functional tests
+  config.include Devise::TestHelpers, type: :controller
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
